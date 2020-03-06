0978ed5cab24b930944fac7cdd10cf3bbe493fe0
CLAUDIA MULIA <claudiavembryani@gmail.com>
2020-03-06 09:33:09 +0000

    remove .git

---
+++ week_09/day_3/hello_world/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    // React.createElement("h1",null, "Hello World!!!!!") // not much simpler
+    <h1>Holaaa World!!!!!! </h1>
+  );
+}
+
+export default App;
